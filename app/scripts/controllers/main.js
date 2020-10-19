29ebc1a6ec752688c1fa6980ac8bda14232418ce
jamesmudgett <jmm.seasons@gmail.com>
2020-10-19 10:52:07 -0500

    update social images

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -308,6 +308,8 @@ function($rootScope, $scope, $routeParams, $location, $timeout, $q) {
   };
 
   $scope.DownloadPicture = function() {
+    var theme = window.util.hash('theme') || 'default';
+    _paq.push(['trackEvent', 'download', theme]);
 
     $scope.generatePicture()
     .then(function(canvas) {
@@ -329,6 +331,9 @@ function($rootScope, $scope, $routeParams, $location, $timeout, $q) {
   };
 
   $scope.GetPro = function() {
+    var theme = window.util.hash('theme') || 'default';
+    _paq.push(['trackEvent', 'clicked pro', theme]);
+
     stripe
       .redirectToCheckout({
         lineItems: [
@@ -336,24 +341,10 @@ function($rootScope, $scope, $routeParams, $location, $timeout, $q) {
         ],
         mode: 'payment',
         successUrl: 'https://virtualbackgroundpro.com/#/?payment=success',
-        cancelUrl: 'https://virtualbackgroundpro.com/#/?payment=canceled',
+        cancelUrl: 'https://virtualbackgroundpro.com/',
       })
       .then(function(result) {
-        // If `redirectToCheckout` fails due to a browser or network
-        // error, display the localized error message to your customer
-        // using `result.error.message`.
-        if (!result.error.message) {
-          model.store.pro = true;
-          var watermark = document.querySelector('.watermark-logo');
-          if (watermark) {
-            watermark.remove();
-          }
-
-          var probutton = document.querySelector('.button-pro');
-          if (probutton) {
-            probutton.remove();
-          }
-        }
+        model.store.pro = false;
       });
   };
 
@@ -374,6 +365,11 @@ function($rootScope, $scope, $routeParams, $location, $timeout, $q) {
       if (watermark) {
         watermark.remove();
       }
+
+      var probutton = document.querySelector('.button-pro');
+      if (probutton) {
+        probutton.remove();
+      }
     }
 
     // hack to prevent automatic scrolling from contenteditable
@@ -392,5 +388,57 @@ function($rootScope, $scope, $routeParams, $location, $timeout, $q) {
     }
 
   });
+
+  var themeActive = '';
+  var $themeActive;
+  var $container = document.querySelector('.themes-box');
+
+  // if we're on a page without the themes
+  if (!$container) {
+    return;
+  }
+
+  function hashChange () {
+    if (window.util.hash('payment')) {
+      var theme = window.util.hash('theme') || 'default';
+
+      _paq.push(['trackEvent', 'pro', theme]);
+
+      model.store.pro = true;
+      var watermark = document.querySelector('.watermark-logo');
+      if (watermark) {
+        watermark.remove();
+      }
+  
+      var probutton = document.querySelector('.button-pro');
+      if (probutton) {
+        probutton.remove();
+      }
+    }
+
+    themeActive = window.util.hash('theme');
+
+    // if no theme in the url
+    if (!themeActive) {
+      // select the first in the list
+      $container.querySelector('.theme-item').click();
+
+      return;
+    }
+
+    // uncheck previous active theme
+    $themeActive = $container.querySelector('.active');
+    if ($themeActive) {
+      window.util.removeClass($themeActive, 'active');
+    }
+
+    $themeActive = $container.querySelector('.js-' + themeActive);
+    if ($themeActive) {
+      window.util.addClass($themeActive, 'active');
+    }
+  }
+
+  hashChange();
+  window.addEventListener('hashchange', hashChange, false);
 
 }]);
